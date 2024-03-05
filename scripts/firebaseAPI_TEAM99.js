4a9c3621d8f29bedc31e72c59b89fec3e0cc4a6f
GURVEER <graith@my.bcit.ca>
2024-03-05 09:54:11 -0800

    Demo 09 commit push

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyAQpHeWrEGrbptdzyUEVmhFNtDKm60-F2o",
+    authDomain: "comp1800-202410-demo-e398f.firebaseapp.com",
+    projectId: "comp1800-202410-demo-e398f",
+    storageBucket: "comp1800-202410-demo-e398f.appspot.com",
+    messagingSenderId: "1059038512329",
+    appId: "1:1059038512329:web:f3886d5a1508cad7d4b918"
 };
 
 //--------------------------------------------
